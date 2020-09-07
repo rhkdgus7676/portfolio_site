8485b685a9bfcb930acd9cb56b07ed1e460f1873
Kimkwanghyun <66558399+rhkdgus7676@users.noreply.github.com>
2020-09-07 16:56:47 +0900

    Add files via upload
    added menu links to part(scrolls to where clicked from the menu)function, scrolls to top function

--- indexpage.js
+++ indexpage.js
@@ -1,2 +1,23 @@
 'use strict';
-// alert('hello');
+
+
+// practice button to scroll to top
+//Get the button:
+const mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down ???px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari(practiceneeded)
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
